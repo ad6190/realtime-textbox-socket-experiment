13b7185574605e344d4e21cb0f5a93b7ff45e479
Aditi Bhatnagar <aditi@rohits-MacBook-Pro.local>
2016-09-18 17:57:31 +0530

    changed url to sample

--- step_1/app.py
+++ step_1/app.py
@@ -1,18 +1,34 @@
-from  gevent import monkey
+from gevent import monkey
 monkey.patch_all()
 
+import cgi
 import redis
-from flask import Flask, render_template
-from flask.ext.socketio import SocketIO
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO
 
-app= Flask(__name__)
+app = Flask(__name__)
 db = redis.StrictRedis('localhost', 6379, 0)
 socketio = SocketIO(app)
+db.set('text', '')
 
-@app.route('/')
-def main():
-	c = db.incr('counter')
-	return render_template('main.html', counter=c)
+
+@app.route('/sample')
+def sample():
+    return render_template('sample.html')
+
+
+@socketio.on('connect', namespace='/pypy')
+def ws_conn():
+    c = db.get('text')
+    socketio.emit('text', {'text': cgi.escape(c)},
+                  namespace="/pypy")
+
+
+@socketio.on('text', namespace='/pypy')
+def ws_sample(message):
+    db.set('text', message['text'])
+    socketio.emit('text', {'text': cgi.escape(message['text'])},
+                  namespace="/pypy", broadcast=True)
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    socketio.run(app)
